4f68d76a587c760ad7748e80d1c5f3beff2511ec
jpan326 <jpan326@gatech.edu>
2024-03-24 10:41:44 +0800

    vote module added

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
+import time
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from .config import settings
+# 导入settings，并在后面引用settings.XXXX
 
 # 为了用SQLAlchemy才做的这个文件，see in https://fastapi.tiangolo.com/tutorial/sql-databases/
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:jiajia@localhost/fastapi"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:jiajia@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 # 这个engine是用来建立连接的
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -24,3 +30,36 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# postgresql 使用：可以自己创建一个server，里面创建很多database，database里面有很多table (schemas --> table)，可以view data
+# 我们要用psycopg去在python上运行postgresql，因此要pip install psycopg2-binary
+# (see in https://www.psycopg.org/docs/install.html#quick-install)
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="fastapi",
+            user="postgres",
+            password="jiajia",
+            cursor_factory=RealDictCursor,
+        )
+        # cursor_factory=RealDictCursor 是针对 psycopg2 库的参数设置
+        # 用于创建一个“真实字典游标”（RealDictCursor）。当使用该参数时，查询结果将以字典的形式返回，
+        # 其中键是列名，值是对应列的值。这样的设置能够使得查询结果更易于使用，并且提供了更直观的方式来处理结果集中的数据。
+        cursor = conn.cursor()
+        print("Database connection was successful!")
+        break
+    except Exception as error:
+        print("Error: ", error)
+        time.sleep(
+            2
+        )  # 加上外面的while循环，以及完成时的break，构成了不断去连数据库的一个操作，2秒连一次
+
+# psycopg仍然是建立在SQL语言上的，而SQL Alchemy是ORM（Object Retional Mapper），直接用面向对象的语言去查询数据库
+# 对象关系映射：将数据库表、列、关系等元素映射到面向对象的类、属性和关系。
+# 查询语言：提供一种高级别的查询语言，使开发人员可以使用面向对象的语法来查询数据库，而无需直接编写SQL语句。
+
+# 而且，记住！我们在用psycopg的时候，我们的table是手动在postgresql里面搭的，但SQLAlchemy里面，我们可以通过编程自动生成table
+
+my_posts = [{"title": "title1", "content": "content1", "published": True, "id": 1}]
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,8 @@
 # 建立routers文件夹，把归属于一个大类的路由端点放在同一个文件里
 from fastapi import FastAPI, HTTPException, Response, status, Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
+from sqlalchemy import func
 from .. import models, schemas, utils, oauth2
 from ..database import get_db
 
@@ -12,15 +13,21 @@ router = APIRouter(prefix="/posts", tags=["Posts"])
 # 加了tags，注意是list，就会在docs里面自动分组
 
 
-@router.get("/", response_model=List[schemas.Post])
+# @router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
+# @router.get("/")
 def get_posts(
-    db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)
+    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),limit: int = 20,
+    skip: int = 0, search: Optional[str] = ""
 ):
+    # limit表示列出的post数量限制变量，默认为10，如果网址后缀注明?limit=3，那么就是3
+    # 多个参数限制，那就加&
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     # print(posts)
-    posts = db.query(models.Post).all()
-    return posts
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id==models.Post.id, isouter=True).group_by(models.Post.id)
+    return results
 
 
 @router.post(
@@ -31,7 +38,7 @@ def get_posts(
 def create_posts(
     post: schemas.PostCreate,
     db: Session = Depends(get_db),
-    user_id: int = Depends(oauth2.get_current_user),
+    current_user: int = Depends(oauth2.get_current_user),
 ):
     # 这里Depends(oauth2.get_current_user)要求必须要login才可以post，在postman里面就是Authorization里面输入login里返回的token，或者header里面输入 Bearer XXXX(一长串token)
     # print(new_post) #用途不大，在terminal里面显示，只有写在return里面，才会真正在网页里面显示
@@ -52,8 +59,8 @@ def create_posts(
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
     # ** 是unpack的意思，就可以按照dict的内容去导入
     # Basemodel 后面可以直接用.dict()去变成字典，但是该方法将被弃用，可以用model_dump()去实现
-    print(user_id)
-    new_post = models.Post(**post.model_dump())
+    # print(current_user.id)
+    new_post = models.Post(owner_id = current_user.id, **post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -64,7 +71,7 @@ def create_posts(
 def get_post(
     id: int,
     db: Session = Depends(get_db),
-    user_id: int = Depends(oauth2.get_current_user),
+    current_user: int = Depends(oauth2.get_current_user),
 ):
     # my_post =find_by_id(id)
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""",str(id))
@@ -76,16 +83,16 @@ def get_post(
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"cannot find post {id}"
         )
-    else:
-        return post
+    
+    return post
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def put_post(
+def update_post(
     id: int,
     updated_post: schemas.PostCreate,
     db: Session = Depends(get_db),
-    user_id: int = Depends(oauth2.get_current_user),
+    current_user: int = Depends(oauth2.get_current_user),
 ):
     # my_post =find_by_id(id)
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s RETURNING *""",
@@ -94,10 +101,13 @@ def put_post(
     # conn.commit()
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
-    if not post_query.first():
+    post = post_query.first()
+    if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"cannot find post {id}"
         )
+    if post.owner_id != int(current_user.id):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
 
     post_query.update(updated_post.model_dump(), synchronize_session=False)
     # 像delete update可以直接在post_query上操作，非常简单
@@ -111,23 +121,28 @@ def put_post(
     #     return {f"this is the updated post {id}": my_post}
 
 
-@router.delete("/{id}")
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(
     id: int,
     db: Session = Depends(get_db),
-    user_id: int = Depends(oauth2.get_current_user),
+    current_user: int = Depends(oauth2.get_current_user),
 ):
     # my_post =find_by_id(id)
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", str(id))
     # deleted_post = cursor.fetchone()
     # conn.commit()
 
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if not post.first():
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"cannot find post {id}"
         )
-    post.delete(synchronize_session=False)
+    
+    if post.owner_id != int(current_user.id):
+        return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+    
+    post_query.delete(synchronize_session=False)
     db.commit()
     # my_posts.remove(my_post)
     # return {f"this is the deleted post {id}": deleted_post}
